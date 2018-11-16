1b2b55721d34be364922c62cfd8a0e7ce7919f33
sixone <931039665@qq.com>
2018-11-16 20:49:05 +0800

    信息更新

---
+++ index.js
@@ -0,0 +1,59 @@
+module.exports = {
+	// 模版标识
+	name: 'finance-three',
+	// 模版引擎
+	engine: 'pug',
+
+	// 页面
+	pages: [
+		{
+			name: '首页',
+			// 路径
+			path: '/',
+			// 预览图
+			thumbnail: 'home.png',
+		},
+		{
+			name: '公司简介',
+			// 路径
+			path: 'about',
+			// 预览图
+			thumbnail: 'about.png',
+		},
+		{
+			name: '我们的业务',
+			// 路径
+			path: 'product',
+			// 预览图
+			thumbnail: 'product.png',
+		},
+		{
+			name: '新闻中心',
+			// 路径
+			path: 'news',
+			// 预览图
+			thumbnail: 'news.png',
+		},
+		{
+			name: '加入启融',
+			// 路径
+			path: 'join',
+			// 预览图
+			thumbnail: 'join.png',
+		},
+		{
+			name: '合作伙伴',
+			// 路径
+			path: 'partner',
+			// 预览图
+			thumbnail: 'partner.png',
+		},
+		{
+			name: '联系我们',
+			// 路径
+			path: 'contact',
+			// 预览图
+			thumbnail: 'contact.png',
+		}
+	]
+}
